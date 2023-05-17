0ce48a028072b2630292aadc7a26e74f43bc7d2a
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2023-05-17 14:58:17 -0300

    refactor: running prettier plugin tailwindcss

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,6 +1,6 @@
 export const Hello = () => {
     return (
-        <div className="flex flex-col gap-2 text-zinc-200 text-xl font-medium text-center">
+        <div className="flex flex-col gap-2 text-center text-xl font-medium text-zinc-200">
             <h1 className="text-3xl">Hello World 🥳</h1>
 
             <p>
@@ -10,7 +10,7 @@ export const Hello = () => {
             <p className="mt-5">
                 Made with 🤍 by{" "}
                 <a
-                    className="inline-block underline underline-offset-[2px] transition-all ease-in-out duration-200 hover:-translate-y-[2px] hover:text-purple-400"
+                    className="inline-block underline underline-offset-[2px] transition-all duration-200 ease-in-out hover:-translate-y-[2px] hover:text-purple-400"
                     href="https://www.github.com/arthurlbo"
                     target="_blank"
                     rel="noreferrer"
--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -1,6 +1,6 @@
 export const Wrapper = ({ children }: { children: React.ReactNode }) => {
     return (
-        <main className="relative flex min-h-screen max-w-full w-full flex-col items-center justify-center bg-background">
+        <main className="relative flex min-h-screen w-full max-w-full flex-col items-center justify-center bg-background">
             {children}
         </main>
     );
