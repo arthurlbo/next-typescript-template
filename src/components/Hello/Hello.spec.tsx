5cca50443249b53813778cd3489663c5cf848552
arthurlbo <arthurlbo16@gmail.com>
2023-07-05 21:23:56 -0300

    refactor: improve hello component

--- src/components/Hello/Hello.spec.tsx
+++ src/components/Hello/Hello.spec.tsx
@@ -1,8 +1,21 @@
 import { Hello } from "./Hello";
 
 describe("<Hello />", () => {
-    it("should renders correctly", () => {
+    beforeEach(() => {
         cy.mount(<Hello />);
-        cy.get("h1").contains("Hello");
+    });
+    it("should render the component correctly", () => {
+        cy.contains("Hello Dev 🥳").should("be.visible");
+
+        cy.contains("Get started by editing").should("be.visible");
+        cy.contains("src/app/page.tsx").should("be.visible");
+
+        cy.contains("Arthur Lobo").should("have.attr", "href", "https://www.github.com/arthurlbo");
+    });
+    it("should open the link in a new tab", () => {
+        cy.contains("Arthur Lobo")
+            .should("have.attr", "href", "https://www.github.com/arthurlbo")
+            .and("have.attr", "target", "_blank")
+            .and("have.attr", "rel", "noreferrer");
     });
 });
--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,14 +1,15 @@
 export const Hello = () => {
     return (
-        <div className="flex flex-col gap-2 text-center text-xl font-medium text-zinc-200">
-            <h1 className="text-3xl">Hello World 🥳</h1>
+        <div className="z-10 flex flex-col gap-5 text-center text-xl font-medium text-zinc-200">
+            <h1 className="text-3xl font-bold">Hello Dev 🥳</h1>
 
-            <p>
-                Get started by editing <code className="font-mono text-sm font-bold">src/app/page.tsx</code>
-            </p>
+            <div className="flex items-center gap-2 rounded-full border border-zinc-800 bg-zinc-100/5 px-3 py-0.5 leading-none drop-shadow-lg backdrop-blur hover:border-zinc-700">
+                <p className="text-lg">Get started by editing</p>
+                <code className="font-mono text-sm font-bold">src/app/page.tsx</code>
+            </div>
 
             <p className="mt-5">
-                Made with 🤍 by{" "}
+                Made with 💜 by{" "}
                 <a
                     className="inline-block underline underline-offset-[2px] transition-all duration-200 ease-in-out hover:-translate-y-[2px] hover:text-purple-400"
                     href="https://www.github.com/arthurlbo"
