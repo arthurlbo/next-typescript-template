938342a8b5bb5f204eaf7fe59c252644d9542e14
arthurlbo <arthurlbo16@gmail.com>
2023-07-05 21:19:41 -0300

    chore: remove storybook

--- src/components/Hello/Hello.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryFn } from "@storybook/react";
-import { Hello } from "./Hello";
-
-export default {
-    title: "Hello",
-    component: Hello,
-} as Meta;
-
-export const Default: StoryFn = () => <Hello />;
