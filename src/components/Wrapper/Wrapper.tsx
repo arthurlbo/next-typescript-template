ce53640b0cd74c0c2132174f43523c47b4308423
arthurlbo <arthurlbo16@gmail.com>
2023-07-05 21:18:42 -0300

    chore: remove wrapper component

--- src/components/Wrapper/Wrapper.tsx
+++
@@ -1,7 +0,0 @@
-export const Wrapper = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <main className="relative flex min-h-screen w-full max-w-full flex-col items-center justify-center bg-background">
-            {children}
-        </main>
-    );
-};
