7b68a53635fde8c002b93c90447ddc7f1dd530c8
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2023-10-06 10:35:54 -0300

    chore: updating config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,19 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+    screenshotOnRunFailure: false,
+    video: false,
     component: {
         devServer: {
             framework: "next",
             bundler: "webpack",
         },
-        specPattern: ["**/*.cy.{js,jsx,ts,tsx}", "**/*.spec.{js,jsx,ts,tsx}", "**/*.test.{js,jsx,ts,tsx}"],
-        screenshotOnRunFailure: false,
+        specPattern: ["**/*.spec.{js,jsx,ts,tsx}"],
     },
     e2e: {
-        setupNodeEvents(on, config) {
-            // implement node event listeners here
-        },
+        baseUrl: "http://localhost:3000",
+        specPattern: ["**/*.spec.{js,jsx,ts,tsx}"],
+        viewportHeight: 1080,
+        viewportWidth: 1920,
     },
-    video: false,
 });
